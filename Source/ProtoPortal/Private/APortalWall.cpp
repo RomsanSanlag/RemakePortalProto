499e3d5f64564ebe3149afe185f95e087bc43684
Romsan <santinoromero77400@gmail.com>
2024-11-26 00:19:16 +0100

    setup portal wall mat + constuction script for editor with variable that can be modified with the editor

---
+++ Source/ProtoPortal/Private/APortalWall.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProtoPortal/Public/APortalWall.h"
+
+
+// Sets default values
+AAPortalWall::AAPortalWall()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AAPortalWall::BeginPlay()
+{
+	Super::BeginPlay();
+	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, "Begin play");
+}
+
+// Called every frame
+void AAPortalWall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/ProtoPortal/Public/APortalWall.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "APortalWall.generated.h"
+
+UCLASS()
+class PROTOPORTAL_API AAPortalWall : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AAPortalWall();
+
+	UPROPERTY(BlueprintReadOnly, EditInstanceOnly)
+	float WallWidth = 100.f;
+	
+	UPROPERTY(BlueprintReadOnly, EditInstanceOnly)
+	float WallHeight = 100.f;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
