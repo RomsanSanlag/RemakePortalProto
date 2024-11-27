808550a3c707b28f6106bbe24148bd2fef0c5a86
Romsan <santinoromero77400@gmail.com>
2024-11-27 11:58:27 +0100

    creation portal actor component et début de gestion du line trace

---
+++ Source/ProtoPortal/Private/ACPortalComponent.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ACPortalComponent.h"
+
+#include "APortalWall.h"
+#include "CollisionDebugDrawingPublic.h"
+
+
+// Sets default values for this component's properties
+UACPortalComponent::UACPortalComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UACPortalComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UACPortalComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                       FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UACPortalComponent::SpawnPortal(FVector StartLocation, FVector Direction, bool PortalA)
+{
+	float MaxDistanceSpawnPortal = 1000.f;
+	FVector EndLocation = StartLocation + Direction * MaxDistanceSpawnPortal;
+	TArray<FHitResult> HitResults;
+	
+	DrawLineTraces(GetWorld(),StartLocation,EndLocation,HitResults,1);
+
+	//for(FHitResult HitResult : HitResults)
+	//{
+	//	Cast<FHitResult, AAPortalWall>();
+	//	if (HitResult == AAPortalWall)
+	//	{
+	//		
+	//	}
+	//}
+}
+
+
---
+++ Source/ProtoPortal/Public/ACPortalComponent.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ACPortalComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROTOPORTAL_API UACPortalComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UACPortalComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void SpawnPortal(FVector StartLocation, FVector Direction, bool PortalA);
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
